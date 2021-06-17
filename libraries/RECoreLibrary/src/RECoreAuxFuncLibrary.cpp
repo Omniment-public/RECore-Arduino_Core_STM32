90564476b0ce30bfeec02512355bfd336f67abad
r-tamura-omniment <rui-tamura@omniment.co.jp>
2021-06-18 00:25:37 +0900

    [update]電圧測定と内蔵温度のプログラム修正

--- libraries/RECoreLibrary/src/RECoreAuxFuncLibrary.cpp
+++ libraries/RECoreLibrary/src/RECoreAuxFuncLibrary.cpp
@@ -13,19 +13,22 @@
 
 #include "RECoreAuxFuncLibrary.h"
 
-float RECoreAuxFuncLibrary::getBatteryVoltage(){
+float RECoreAuxFuncLibrary::getBatteryVoltage()
+{
     //(((3.3 / 1023) * vbat_adc) + 0.35) * ((120.0 + 51.0) / 51.0)
     float vbat_adc = analogRead(A8);
-    return (3.35294 * ( (0.00322581 * vbat_adc) + 0.35));
+    return ((0.00322581 * vbat_adc) + 0.1 + 0.35) * 3.156862745;
 }
 
-int RECoreAuxFuncLibrary::getMcuTemp(){
+int RECoreAuxFuncLibrary::getMcuTemp()
+{
+    //V30 : 0.76
+    //avg_slope : 2.5
     //10bit adc
-    // (1.43 - (adc * 3.3/1023))/0.0043 + 30
+    // (0.76-(adc * 3.3/1023))/2.5 + 30
     uint16_t adc_val = analogRead(ATEMP);
-    float calc_temp = (232.558 * (1.43 - (0.00322581 * adc_val))) + 30;
+    float calc_temp = 30.304 - (0.00129032 * adc_val);
     return calc_temp;
 }
 
 #endif // ARDUINO_ARCH_STM32
-
