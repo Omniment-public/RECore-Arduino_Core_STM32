f3c76e710890595bbec47beb5854d3b871499709
r-tamura <rui-tamura@omniment.co.jp>
2021-08-18 03:50:46 +0900

    [update]ステッピングのexample修正

--- libraries/RECoreLibrary/examples/Stepping_example/Stepping_example.ino
+++ libraries/RECoreLibrary/examples/Stepping_example/Stepping_example.ino
@@ -35,7 +35,7 @@ void loop() {
   //dir : true , false
 
   //example set MOTOR_UNIT_A 200min^-1
-  recore.setSteppingSpeed(MOTOR_UNIT_A, 200, true);
+  recore.setSteppingSpeed(MOTOR_UNIT_A, 100, true);
   
   delay(1000);
   recore.setSteppingSpeed(MOTOR_UNIT_A, 0, true);
@@ -46,6 +46,6 @@ void loop() {
   //motor unit : MOTOR_UNIT_A , MOTOR_UNIT_B
   //set speed : set min^-1
   //rotate value : int32_t
-  recore.setSteppingSteps(MOTOR_UNIT_A, 200, 200);
+  recore.setSteppingSteps(MOTOR_UNIT_A, 100, 200);
   delay(1000);
 }
